7e51284fed878eec7ca9a36d917e18d2d294dcb5
Anar Mamedovi <84316960+PandaLoop-Anar@users.noreply.github.com>
2022-05-17 01:33:33 +0400

    Add files via upload

---
+++ functions/createApple.js
@@ -0,0 +1,10 @@
+import { MOVING_AREA_HEIGHT, MOVING_AREA_WIDTH, CUBE_SIDE } from "../config/config.js";
+
+export const createApple = () => {
+  const apple = document.getElementById("apple");
+  const randomLeft = Math.trunc(Math.random() * (MOVING_AREA_WIDTH / CUBE_SIDE)) * CUBE_SIDE;
+  const randomTop = Math.trunc(Math.random() * (MOVING_AREA_HEIGHT / CUBE_SIDE)) * CUBE_SIDE;
+  apple.style.left = `${randomLeft}px`;
+  apple.style.top = `${randomTop}px`;
+  apple.style.display = "block";
+};
