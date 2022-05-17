078fa3472b9c42346e69fc934f6d1758fa3d420e
Anar Mamedovi <84316960+PandaLoop-Anar@users.noreply.github.com>
2022-05-18 01:57:48 +0400

    Update createApple.js

--- functions/createApple.js
+++ functions/createApple.js
@@ -1,10 +1,20 @@
 import { MOVING_AREA_HEIGHT, MOVING_AREA_WIDTH, CUBE_SIDE } from "../config/config.js";
+import { eatApple, snakeCoordinates } from "../index.js";
 
 export const createApple = () => {
   const apple = document.getElementById("apple");
   const randomLeft = Math.trunc(Math.random() * (MOVING_AREA_WIDTH / CUBE_SIDE)) * CUBE_SIDE;
   const randomTop = Math.trunc(Math.random() * (MOVING_AREA_HEIGHT / CUBE_SIDE)) * CUBE_SIDE;
+  
+  // for(let i=1; i<snakeCoordinates.length; i++){
+  //   if(snakeCoordinates[i].left===randomLeft && snakeCoordinates[i].top===randomTop){
+  //     eatApple();
+  //     console.log('same');
+  //   }
+  // }
+
   apple.style.left = `${randomLeft}px`;
   apple.style.top = `${randomTop}px`;
   apple.style.display = "block";
+  return [randomLeft, randomTop];
 };
