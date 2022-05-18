c1c3f2c119172a8d268262962630d0ca04280601
Anar Mamedovi <84316960+PandaLoop-Anar@users.noreply.github.com>
2022-05-19 02:28:08 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import { snake } from "./modules/snake.module.js";
-import { changeDirection } from "./functions/changeDirection.js";
+import { changeDirection, intervalId } from "./functions/changeDirection.js";
 import { createApple } from "./functions/createApple.js";
 import { checkApple } from "./functions/checkApple.js";
+import { move } from "./functions/move.js";
+import { CUBE_SIDE } from "./config/config.js";
 
 const idOfSnake = document.getElementById("snake-cubes");
 const userScoreContainer = document.getElementById("user-score_num");
 const bestScoreContainer = document.getElementById("best-score_num");
+const toggleBtn = document.getElementById("toggle-btn");
 
 let bestScore = 0;
 let userScore = 0;
@@ -27,10 +30,10 @@ export let snakeCoordinates = [
 export let initialSnake = new snake(idOfSnake, snakeCoordinates);
 // initialSnake.render();
 // // moving snake
-changeDirection();
+// changeDirection();
 
 // creating food
-export let foodCoordinates = createApple();
+export let foodCoordinates;
 
 // check apple is in the mouth of snake or not
 // checkApple();
@@ -52,6 +55,10 @@ const addNewSegment = () => {
   });
 };
 
+export let stopFunction = (intervalId) => {
+  console.log(intervalId);
+};
+
 export const gameOver = () => {
   const finalScoreContainer = document.getElementById("final-score_num");
   const darkBackground = document.querySelector(".dark-background");
@@ -67,6 +74,9 @@ export const gameOver = () => {
     if (userScore > bestScore) {
       bestScore = userScore;
     }
+    stopFunction = (intervalId) => {
+      clearInterval(intervalId);
+    };
     startGame();
 
     darkBackground.style.display = "none";
@@ -89,18 +99,48 @@ const startGame = () => {
   // moving snake
   foodCoordinates = createApple();
   checkApple();
+  changeDirection();
 
   // let snake = new snake();
-
   // move
   // change Direction
   // check apple
-
   // die
 };
-startGame();
+
+// const toggleBtnFunction = () => {
+  let toggleNum=0;
+  toggleBtn.addEventListener("click", (e) => {
+    if (toggleNum === 0) {
+      e.target.innerText = "Pause Game";
+      e.target.style.backgroundColor = "orange";
+      startGame();
+    } else if (toggleNum > 1 && toggleNum % 2 === 0) {
+      e.target.innerText = "Pause Game";
+      e.target.style.backgroundColor = "orange";
+      stopFunction = (intervalId) => {
+        return;
+      };
+      changeDirection();
+    } else if (toggleNum % 2 === 1) {
+      e.target.innerText = "Start Game";
+      e.target.style.backgroundColor = "#24d324";
+
+      stopFunction = (intervalId) => {
+        clearInterval(intervalId);
+      };
+      stopFunction(intervalId);
+    }
+    toggleNum++;
+  });
+// };
+// toggleBtnFunction(0);
 
 // createSnake
 // createApple
 
 // setInterval(game, 100)
+// setInterval(changeDirection, 1000);
+// imodzraos tviton, bolo poziciis mimartulebit
+
+// clearInterval
