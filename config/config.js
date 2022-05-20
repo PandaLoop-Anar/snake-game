f90124ade95a0bec2f917fde1070fd6423204d61
Anar Mamedovi <84316960+PandaLoop-Anar@users.noreply.github.com>
2022-05-20 15:12:13 +0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,5 +1,12 @@
 
 export const CUBE_SIDE = 30;
-export const MOVING_AREA_WIDTH=960;
-export const MOVING_AREA_HEIGHT=450;
+export let MOVING_AREA_WIDTH=960;
+export let MOVING_AREA_HEIGHT=450;
 export const SNAKE_SPEED=200;
+
+export const changeAreaWidth=(width)=>{
+    MOVING_AREA_WIDTH=width;
+}
+export const changeAreaHeight=(height)=>{
+    MOVING_AREA_HEIGHT=height;
+}
